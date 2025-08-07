1b938c1af49085dcec531018db8233c9f75090ad
zaidan16-droid <zaidanshafi6@gmail.com>
2025-08-07 10:06:37 +0700

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,5 +1,11 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
+   try {
+    
+  } catch (err) {
+    console.error('Error:', err);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+} if (req.method !== 'POST') return res.status(405).end();
 
   const { status, INV123, is_closed_payment } = req.body;
 
